ef78e6b6265cfc204b7e610b0157f8912813e796
mhristev <502270@student.fontys.nl>
2022-11-26 14:15:03 +0100

    structure

--- StudentHousingBV/forms/Form1.cs
+++ StudentHousingBV/forms/Form1.cs
@@ -1,3 +1,5 @@
+using StudentHousingBV.models;
+
 namespace StudentHousingBV
 {
     public partial class Form1 : Form
@@ -5,8 +7,6 @@ namespace StudentHousingBV
         public Form1()
         {
             InitializeComponent();
-            Console.WriteLine("Hello World!");
-
         }
     }
 }
